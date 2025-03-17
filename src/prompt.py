747af5a3135e525280819221fdb331d814d03d96
muntahaaa <bsse1450@iit.du.ac.bd>
2025-03-17 12:37:10 +0600

    chatbot updated

--- src/prompt.py
+++ src/prompt.py
@@ -1,1 +1,8 @@
- 
+system_prompt = (
+    "You are an assistant for question-answering tasks."
+    "Use the following information to answer the questions."
+    "If you don't know the answer, say that you do not have enough information."
+    "Use three sentences to answer the question and keep them concise."
+    "\n\n"
+    "{context:} "
+)
